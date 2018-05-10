bd7b047ec68ba8500771fc5e739f41b2fd8d35c1
rlui94 <ryanlui.college@gmail.com>
2018-05-10 10:34:00 -0700

    Renamed variables, cleaned if statements, standard exceptions

--- Evebuilds.py
+++ Evebuilds.py
@@ -3,95 +3,83 @@ import json
 import time
 import threading
 import _thread
+import logging
 import esipy
 from esipy import EsiClient
 from esipy import App
 
 
-def getdata(threadName, delay, shipdict):
+def getdata(thread_name, delay, shipdict):
     while True:
         r = requests.get('https://redisq.zkillboard.com/listen.php')
-        test = r.json()
+        killmail = r.json()
         try:
-            t = test["package"]["killmail"]["victim"]["items"]
-            ship = test["package"]["killmail"]["victim"]["ship_type_id"]
-            if ship in shipdict:
-                shipslots = shipdict.get(ship)
-                for x in t:
+            km = killmail["package"]["killmail"]["victim"]["items"]
+            ship_id = killmail["package"]["killmail"]["victim"]["ship_type_id"]
+            if ship_id in shipdict:
+                ship = shipdict.get(ship_id)
+                for x in km:
                     flag = True
-                    j = x["item_type_id"]
-                    l = x["flag"]
-                    if l >= 11 and l < 19:
-                      mydict = shipslots[0]
-                    elif l >= 19 and l < 26:
-                      mydict = shipslots[1]
-                    elif l >= 27 and l <= 34:
-                      mydict = shipslots[2]
-                    elif l >= 92 and l <= 94:
-                      mydict = shipslots[3]
-                    else:
-                      flag = False
-                    if (j in mydict) and flag:
-                        mydict["count"] = mydict["count"] + 1
-                        z = mydict.get(j)
-                        mydict[j] = z + 1
-                    else:
-                        mydict["count"] = mydict["count"] + 1
-                        mydict[j] = 1
-
-            else:
-                p = 0
-                shipdict[ship] = [{}, {}, {}, {}]
-                shipslots = shipdict.get(ship)
-                flag = True
-                for x in t:
-                    j = x["item_type_id"]
-                    l = x["flag"]
-                    if l >= 11 and l <= 18:
-                        mydict = shipslots[0]
-                    elif l >= 19 and l < 26:
-                        mydict = shipslots[1]
-                    elif l >= 27 and l <= 34:
-                        mydict = shipslots[2]
-                    elif l >= 92 and l <= 94:
-                        mydict = shipslots[3]
+                    item_id = x["item_type_id"]
+                    fnum = x["flag"]
+                    if 11 <= fnum <= 18:
+                        currslot = ship[0]
+                    elif 19 <= fnum <= 26:
+                        currslot = ship[1]
+                    elif 27 <= fnum <= 34:
+                        currslot = ship[2]
+                    elif 92 <= fnum <= 94:
+                        currslot = ship[3]
                     else:
                         flag = False
-                    if (j in mydict) and flag:
-                        mydict["count"] = 1
-                        z = mydict.get(j)
-                        mydict[j] = z+1
+                    if (item_id in currslot) and flag:
+                        ship[4] = ship[4] + 1
+                        z = currslot.get(item_id)
+                        currslot[item_id] = z + 1
+                    else:
+                        ship[4] = ship[4] + 1
+                        currslot[item_id] = 1
+
+            else:
+                shipdict[ship_id] = [{}, {}, {}, {}]
+                ship = shipdict.get(ship_id)
+                flag = True
+                for x in km:
+                    item_id = x["item_type_id"]
+                    fnum = x["flag"]
+                    if 11 <= fnum <= 18:
+                        currslot = ship[0]
+                    elif 19 <= fnum <= 26:
+                        currslot = ship[1]
+                    elif 27 <= fnum <= 34:
+                        currslot = ship[2]
+                    elif 92 <= fnum <= 94:
+                        currslot = ship[3]
+                    else:
+                        flag = False
+                    if (item_id in currslot) and flag:
+                        ship[4] = ship[4] + 1
+                        z = currslot.get(item_id)
+                        currslot[item_id] = z+1
                     elif flag:
-                        mydict["count"] = 1
-                        mydict[j] = 1
+                        ship[4] = ship[4] + 1
+                        currslot[item_id] = 1
 
-
-
-            #print(ship)
+            print(ship_id)
             time.sleep(delay)
-        except:
-            print("Null Package")
+        except Exception as e:
+            logging.exception(e)
+
 
 try:
     shipdict = {}
 
     _thread.start_new_thread(getdata, ("Thread-1", 10,shipdict))
 
-except:
-    print('Error: unable to start thread')
+except Exception as e:
+    logging.exception(e)
 while 1:
-    try:
-        test = input("Enter a ship id or print ship list(list): ")
-        if test == "list":
-            shiptest = shipdict
-            for t in shiptest:
-                print(t)
-        else:
-            x = shipdict.get(int(test))
-            test = input("Enter Slot type(0-3): ")
-            y = x[int(test)]
-            for j in y:
-                print(j)
-    except:
-        print("invalid id")
+    test = input("Enter a ship id: ")
+    x = shipdict.get(int(test))
+    print(x)
 
