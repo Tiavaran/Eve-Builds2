8171d8f6d3cb3b88e0709816a1ad0cc878c54e9b
Tiavaran <39042066+Tiavaran@users.noreply.github.com>
2018-05-10 11:40:34 -0700

    Merge branch 'master' of C:\Users\James\PycharmProjects\test with conflicts.

--- Evebuilds.py
+++ Evebuilds.py
@@ -31,6 +31,16 @@ def getdata(thread_name, delay, shipdict):
                     elif 92 <= fnum <= 94:
                         currslot = ship[3]
                     else:
+                      flag = False
+                    #Continue if item is one of our tracked slots.
+                    if flag:
+                        if j in mydict:
+                            mydict["count"] = mydict["count"] + 1
+                            z = mydict.get(j)
+                            mydict[j] = z + 1
+                        else:
+                            mydict["count"] = mydict["count"] + 1
+                            mydict[j] = 1
                         flag = False
                     if (item_id in currslot) and flag:
                         ship[4] = ship[4] + 1
@@ -41,6 +51,20 @@ def getdata(thread_name, delay, shipdict):
                         currslot[item_id] = 1
 
             else:
+                shipdict[ship] = [{}, {}, {}, {}]
+                shipslots = shipdict.get(ship)
+                for x in t:
+                    flag = True
+                    j = x["item_type_id"]
+                    l = x["flag"]
+                    if l >= 11 and l <= 18:
+                        mydict = shipslots[0]
+                    elif l >= 19 and l < 26:
+                        mydict = shipslots[1]
+                    elif l >= 27 and l <= 34:
+                        mydict = shipslots[2]
+                    elif l >= 92 and l <= 94:
+                        mydict = shipslots[3]
                 shipdict[ship_id] = [{}, {}, {}, {}]
                 ship = shipdict.get(ship_id)
                 flag = True
@@ -57,6 +81,19 @@ def getdata(thread_name, delay, shipdict):
                         currslot = ship[3]
                     else:
                         flag = False
+                    if flag:
+                        if j in mydict:
+                            mydict["count"] = mydict["count"] + 1
+                            z = mydict.get(j)
+                            mydict[j] = z+1
+                        else:
+                            if "count" in mydict:
+                                mydict["count"] = mydict["count"] + 1
+                                mydict[j] = 1
+                            else:
+                                mydict["count"] = 1
+                                mydict[j] = 1
+
                     if (item_id in currslot) and flag:
                         ship[4] = ship[4] + 1
                         z = currslot.get(item_id)
@@ -65,6 +102,7 @@ def getdata(thread_name, delay, shipdict):
                         ship[4] = ship[4] + 1
                         currslot[item_id] = 1
 
+            #print(ship)
             print(ship_id)
             time.sleep(delay)
         except Exception as e:
@@ -74,12 +112,28 @@ def getdata(thread_name, delay, shipdict):
 try:
     shipdict = {}
 
-    _thread.start_new_thread(getdata, ("Thread-1", 10,shipdict))
+    _thread.start_new_thread(getdata, ("Thread-1", 10, shipdict))
 
 except Exception as e:
     logging.exception(e)
 while 1:
-    test = input("Enter a ship id: ")
-    x = shipdict.get(int(test))
-    print(x)
+    try:
+        test = input("Enter a ship id or print ship list(list): ")
+        if test == "list":
+            shiptest = shipdict
+            for t in shiptest:
+                print(t)
+        else:
+            x = shipdict.get(int(test))
+            test = input("Enter Slot type(0-3): ")
+            y = x[int(test)]
+            for key, values in y.items():
+                if key == 'count':
+                    print("Total count: " + str(values))
+                else:
+                    divider = y["count"]
+                    percent = values / divider
+                    print(str(key) + ": " + str(percent))
+    except:
+        print("invalid id")
 
