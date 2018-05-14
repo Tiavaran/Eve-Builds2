dd90d0649e536d36d1e9422e11aa952d3d1ed369
Tiavaran <39042066+Tiavaran@users.noreply.github.com>
2018-05-14 14:37:32 -0700

    Updated to include writing and reading data to a JSON file

--- Evebuilds.py
+++ Evebuilds.py
@@ -2,45 +2,58 @@ import requests
 import json
 import time
 import threading
-import _thread
 import esipy
 from esipy import EsiClient
 from esipy import App
 import logging
 
+def print_to_file(shipdict):
+    lock.acquire()
+    with open('data.json', 'w') as fp:
+        fp.truncate()
+        json.dump(shipdict, fp, indent=1)
+    lock.release()
 
-def getdata(threadName, delay, shipdict):
+def load_from_file(shipdict):
+    with open('data.json') as fp:
+        data = json.load(fp)
+    return data
+def getdata(delay, shipdict):
     while True:
         r = requests.get('https://redisq.zkillboard.com/listen.php')
         killmail = r.json()
         try:
             km = killmail["package"]["killmail"]["victim"]["items"]
             ship_id = killmail["package"]["killmail"]["victim"]["ship_type_id"]
+            lock.acquire()
             if ship_id in shipdict:
                 ship = shipdict.get(ship_id)
                 for x in km:
                     flag = True
                     item_id = x["item_type_id"]
                     fnum = x["flag"]
-                    if 11 <= fnum <= 18:
+                    if 11 <= fnum < 19:
                         currslot = ship[0]
-                    elif 19 <= fnum <= 26:
+                    elif 19 <= fnum < 27:
                         currslot = ship[1]
-                    elif 27 <= fnum <= 34:
+                    elif 27 <= fnum < 35:
                         currslot = ship[2]
-                    elif 92 <= fnum <= 94:
+                    elif 92 <= fnum < 95:
                         currslot = ship[3]
                     else:
                       flag = False
                     #Continue if item is one of our tracked slots.
                     if flag:
                         if item_id in currslot:
-                            mydict["count"] = mydict["count"] + 1
-                            z = mydict.get(ship_id)
-                            mydict[ship_id] = z + 1
+                            currslot["count"] = currslot["count"] + 1
+                            currslot[item_id] = currslot[item_id] + 1
                         else:
-                            mydict["count"] = mydict["count"] + 1
-                            mydict[ship_id] = 1
+                            if "count" in currslot:
+                                currslot["count"] = currslot["count"] + 1
+                                currslot[item_id] = 1
+                            else:
+                                currslot["count"] = 1
+                                currslot[item_id] = 1
             else:
                 shipdict[ship_id] = [{}, {}, {}, {}]
                 ship = shipdict.get(ship_id)
@@ -49,30 +62,30 @@ def getdata(threadName, delay, shipdict):
                     flag = True
                     item_id = x["item_type_id"]
                     fnum = x["flag"]
-                    if fnum >= 11 and fnum <= 18:
+                    if fnum >= 11 and fnum < 19:
                         currslot = ship[0]
-                    elif fnum >= 19 and fnum < 26:
-                        mydict = ship[1]
-                    elif fnum >= 27 and fnum <= 34:
-                        mydict = ship[2]
-                    elif fnum >= 92 and fnum <= 94:
-                        mydict = ship[3]
+                    elif fnum >= 19 and fnum < 27:
+                        currslot = ship[1]
+                    elif fnum >= 27 and fnum < 35:
+                        currslot = ship[2]
+                    elif fnum >= 92 and fnum < 95:
+                        currslot = ship[3]
                     else:
                         flag = False
                     if flag:
-                        if item_id in mydict:
-                            mydict["count"] = mydict["count"] + 1
-                            z = mydict.get(item_id)
-                            mydict[item_id] = z+1
+                        if item_id in currslot:
+                            currslot["count"] = currslot["count"] + 1
+                            currslot[item_id] = currslot[item_id] + 1
                         else:
-                            if "count" in mydict:
-                                mydict["count"] = mydict["count"] + 1
-                                mydict[item_id] = 1
+                            if "count" in currslot:
+                                currslot["count"] = currslot["count"] + 1
+                                currslot[item_id] = 1
                             else:
-                                mydict["count"] = 1
-                                mydict[item_id] = 1
+                                currslot["count"] = 1
+                                currslot[item_id] = 1
             #print(ship)
             #print(ship_id)
+            lock.release()
             time.sleep(delay)
         except Exception as e:
             logging.exception(e)
@@ -80,19 +93,27 @@ def getdata(threadName, delay, shipdict):
 
 try:
     shipdict = {}
-
-    _thread.start_new_thread(getdata, ("Thread-1", 10, shipdict))
-
+    shipdict = load_from_file(shipdict)
+    lock = threading.Lock()
+    data_thread = threading.Thread(target=getdata, args=(10, shipdict))
+    data_thread.daemon = True
+    data_thread.start()
 except:
     print('Error: unable to start thread')
 while 1:
     try:
         uinput = input("Enter a ship id or print ship list(list): ")
         if uinput == "list":
+            lock.acquire()
             shiplist = shipdict
             for ship_id in shiplist:
                 print(ship_id)
+            lock.release()
+        elif uinput == "backup":
+            write_thread = threading.Thread(target=print_to_file, args=(shipdict,))
+            write_thread.start()
         else:
+            lock.acquire()
             x = shipdict.get(int(uinput))
             uinput_id = input("Enter Slot type(0-3): ")
             i_list = x[int(uinput_id)]
@@ -103,6 +124,6 @@ while 1:
                     divider = i_list["count"]
                     percent = values / divider
                     print(str(key) + ": " + str(percent))
-    except:
-        print("invalid id")
-
+            lock.release()
+    except Exception as e:
+        logging.exception(e)
