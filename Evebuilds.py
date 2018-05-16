563497a9ad886a406d3e65a01b8e6e8982de46de
rlui94 <eradinkholin@gmail.com>
2018-05-16 12:40:38 -0700

    Spacing to standard, exceptions clarified, cleaned comparisons

--- Evebuilds.py
+++ Evebuilds.py
@@ -7,6 +7,7 @@ from esipy import EsiClient
 from esipy import App
 import logging
 
+
 def print_to_file(shipdict):
     lock.acquire()
     with open('data.json', 'w') as fp:
@@ -14,10 +15,12 @@ def print_to_file(shipdict):
         json.dump(shipdict, fp, indent=1)
     lock.release()
 
+
 def load_from_file(shipdict):
     with open('data.json') as fp:
         data = json.load(fp)
     return data
+
 
 def getdata(delay, shipdict):
     while True:
@@ -63,16 +66,15 @@ def getdata(delay, shipdict):
                 ship = shipdict.get(ship_id)
                 for x in km:
                     flag = True
-                    flag = True
                     item_id = x["item_type_id"]
                     fnum = x["flag"]
-                    if fnum >= 11 and fnum < 19:
+                    if 11 <= fnum < 19:
                         currslot = ship[0]
-                    elif fnum >= 19 and fnum < 27:
+                    elif 19 <= fnum < 27:
                         currslot = ship[1]
-                    elif fnum >= 27 and fnum < 35:
+                    elif 27 <= fnum < 35:
                         currslot = ship[2]
-                    elif fnum >= 92 and fnum < 95:
+                    elif 92 <= fnum < 95:
                         currslot = ship[3]
                     else:
                         flag = False
@@ -99,6 +101,7 @@ def getdata(delay, shipdict):
         except Exception as e:
             logging.exception(e)
 
+
 def check_top_ten(shipslot, itemcount):
     #Indexes 1-10 in each slot dictionary contains the item id of the item that belongs in that slot
     numone = shipslot[10]
@@ -116,18 +119,20 @@ def check_top_ten(shipslot, itemcount):
                 if itemcount > shipslot[shipslot[y]]:
                     return y
 
+
 try:
     shipdict = {}
     try:
         shipdict = load_from_file(shipdict)
-    except:
+    except ValueError:
         print("No Data To Load")
     lock = threading.Lock()
     data_thread = threading.Thread(target=getdata, args=(10, shipdict))
     data_thread.daemon = True
     data_thread.start()
-except:
+except Exception as e:
     print('Error: unable to start thread')
+    logging.exception(e)
 while 1:
     try:
         uinput = input("Enter a ship id or print ship list(list): ")
